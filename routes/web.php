a6f268e376fc978eb1fcd066770c4ce53a296cc1
Hiam-MS <hiam.ms98@gmail.com>
2021-01-14 07:33:50 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/journal', function () {
+    return view('journal');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/addJournal', function () {
+    return view('addJournal');
+});
